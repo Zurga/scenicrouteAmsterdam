4f9d4612589ba65e18de43b095e56a7afd81a19e
Jim <jim.lemmers@gmail.com>
2017-01-19 11:50:28 +0100

    added a TAG and gave the webview javascript abilities, also made sure the webview is shown when the activity is started

--- app/src/main/java/com/jimlemmers/scenicrouteamsterdam/InformationActivity.java
+++ app/src/main/java/com/jimlemmers/scenicrouteamsterdam/InformationActivity.java
@@ -3,10 +3,12 @@ package com.jimlemmers.scenicrouteamsterdam;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.webkit.WebView;
 
 public class InformationActivity extends AppCompatActivity {
     private WebView mWebView;
+    private String TAG = "InformationActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,14 +16,19 @@ public class InformationActivity extends AppCompatActivity {
         setContentView(R.layout.activity_information);
 
         mWebView = new WebView(this);
+        mWebView.getSettings().setJavaScriptEnabled(true);
         Intent intent = getIntent();
         String url = intent.getStringExtra("url");
         String HTML = intent.getStringExtra("HTML");
 
+
         if (url != null) {
+            Log.d(TAG, url);
             mWebView.loadUrl(url);
         } else {
-            mWebView.loadData(HTML, "data/html", "UTF-8");
+            Log.d(TAG, HTML);
+            mWebView.loadData(HTML, "text/html", null);
         }
+        setContentView(mWebView);
     }
 }
