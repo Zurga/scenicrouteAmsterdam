e1d2b29be1527c5cbfb7e2c6d05a5a5c01ae3a92
Jim <jim.lemmers@gmail.com>
2017-01-21 01:03:49 +0100

    added a point class which the POI class can now extend, to seperate the two things a little bit more

--- app/src/main/java/com/jimlemmers/scenicrouteamsterdam/Point.java
+++ app/src/main/java/com/jimlemmers/scenicrouteamsterdam/Point.java
@@ -1,8 +1,28 @@
 package com.jimlemmers.scenicrouteamsterdam;
 
+import com.google.android.gms.maps.model.LatLng;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
 /**
  * Created by jim on 1/19/17.
  */
 
 public class Point {
+    LatLng location;
+
+    public Point(JSONObject pointJSON) {
+        try {
+            String lat = pointJSON.has("lat") ? pointJSON.getString("lat") : null;
+            String lon = pointJSON.has("lng") ? pointJSON.getString("lng") : null;
+            if (lat != null & lon != null) {
+                this.location = new LatLng(Double.parseDouble(lat), Double.parseDouble(lon));
+            } else {
+                this.location = null;
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+    }
 }
