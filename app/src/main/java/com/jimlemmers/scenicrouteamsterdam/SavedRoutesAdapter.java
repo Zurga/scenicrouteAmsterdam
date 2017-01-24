2e8f66af25eb5ebda1f884d88601bfcf02728920
Jim <jim.lemmers@gmail.com>
2017-01-24 12:38:01 +0100

    An adapter for the list of saved routes collected from firebase

--- app/src/main/java/com/jimlemmers/scenicrouteamsterdam/SavedRoutesAdapter.java
+++ app/src/main/java/com/jimlemmers/scenicrouteamsterdam/SavedRoutesAdapter.java
@@ -1,8 +1,46 @@
 package com.jimlemmers.scenicrouteamsterdam;
 
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
 /**
  * Created by jim on 1/23/17.
  */
 
-public class SavedRoutesAdapter {
+public class SavedRoutesAdapter extends ArrayAdapter {
+    public SavedRoutesAdapter(Context context, ArrayList<Route> routes) {
+        super(context, 0, routes);
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent){
+        Route route = (Route) getItem(position);
+        if (convertView == null){
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.route_item, parent, false);
+        }
+
+        convertView.setTag(position);
+        TextView fromTextView = (TextView) convertView.findViewById(R.id.saved_route_from);
+        TextView toTextView = (TextView) convertView.findViewById(R.id.saved_route_to);
+
+        fromTextView.setText(route.fromName);
+        toTextView.setText(route.toName);
+
+        convertView.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View v) {
+                int position = (Integer) v.getTag();
+                Route route = (Route) getItem(position);
+                return false;
+            }
+        });
+        return convertView;
+    }
 }
