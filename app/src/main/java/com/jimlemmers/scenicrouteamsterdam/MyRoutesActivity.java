761085c4640f7a8d8cc8812a26e3a17bb5c3833e
Jim <jim.lemmers@gmail.com>
2017-01-24 12:34:18 +0100

    An Activity which will show the saved routes to the user in a list, work in progress

--- app/src/main/java/com/jimlemmers/scenicrouteamsterdam/MyRoutesActivity.java
+++ app/src/main/java/com/jimlemmers/scenicrouteamsterdam/MyRoutesActivity.java
@@ -5,9 +5,20 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.ListView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class MyRoutesActivity extends AppCompatActivity {
+    String TAG = "MyRoutesActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +27,7 @@ public class MyRoutesActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_user_route_fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -24,6 +35,33 @@ public class MyRoutesActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        SavedRoutesAdapter adapter = new SavedRoutesAdapter(this, getSavedRoutes());
+        ListView listView = (ListView) findViewById(R.id.saved_routes_list);
+        listView.setAdapter(adapter);
     }
 
+    public ArrayList<Route> getSavedRoutes(){
+        //TODO retrieve all the saved routes from the user.
+        final ArrayList<Route> routes = new ArrayList<>();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                Route route = dataSnapshot.getValue(Route.class);
+                routes.add(route);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+        return routes;
+    }
 }
