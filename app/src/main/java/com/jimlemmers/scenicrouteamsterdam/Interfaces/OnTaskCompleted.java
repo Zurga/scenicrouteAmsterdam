30b10c19d468c064e2187a52e8fe87b0edfe79a3
Jim <jim.lemmers@gmail.com>
2017-01-31 12:43:53 +0100

    refactored the onTaskCompleted method to take a Route as argmuent

--- app/src/main/java/com/jimlemmers/scenicrouteamsterdam/Interfaces/OnTaskCompleted.java
+++ app/src/main/java/com/jimlemmers/scenicrouteamsterdam/Interfaces/OnTaskCompleted.java
@@ -1,11 +1,11 @@
 package com.jimlemmers.scenicrouteamsterdam.Interfaces;
 
-import com.jimlemmers.scenicrouteamsterdam.Async.RouteGetter;
+import com.jimlemmers.scenicrouteamsterdam.Classes.Route;
 
 /**
  * Created by jim on 1/21/17.
  */
 
 public interface OnTaskCompleted {
-    void onTaskCompleted(RouteGetter routeGetter);
+    void onTaskCompleted(Route route);
 }
