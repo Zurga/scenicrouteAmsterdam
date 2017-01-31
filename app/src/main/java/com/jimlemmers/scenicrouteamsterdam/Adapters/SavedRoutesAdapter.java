b9e558c7ffc5c75eafc6bd96490f4f4296bc64a3
Jim <jim.lemmers@gmail.com>
2017-01-31 12:40:24 +0100

    Changed onLongClick to onClick and refactored getContext to mContext

--- app/src/main/java/com/jimlemmers/scenicrouteamsterdam/Adapters/SavedRoutesAdapter.java
+++ app/src/main/java/com/jimlemmers/scenicrouteamsterdam/Adapters/SavedRoutesAdapter.java
@@ -2,14 +2,16 @@ package com.jimlemmers.scenicrouteamsterdam.Adapters;
 
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
+import com.google.firebase.database.DatabaseReference;
 import com.jimlemmers.scenicrouteamsterdam.Activities.MapsActivity;
 import com.jimlemmers.scenicrouteamsterdam.Classes.Route;
+import com.jimlemmers.scenicrouteamsterdam.Interfaces.RouteItemSelected;
 import com.jimlemmers.scenicrouteamsterdam.R;
 
 import java.util.ArrayList;
@@ -18,9 +20,10 @@ import java.util.ArrayList;
  * Created by jim on 1/23/17.
  */
 
-public class SavedRoutesAdapter extends ArrayAdapter {
-    public SavedRoutesAdapter(Context context, ArrayList<Route> routes) {
-        super(context, 0, routes);
+public class SavedRoutesAdapter extends RouteAdapter {
+    public SavedRoutesAdapter(Context context, ArrayList<Route> routes, DatabaseReference ref,
+                              RouteItemSelected listener) {
+        super(context, routes, ref, listener);
     }
 
     @Override
@@ -37,19 +40,21 @@ public class SavedRoutesAdapter extends ArrayAdapter {
         fromTextView.setText(route.fromName);
         toTextView.setText(route.toName);
 
-        convertView.setOnLongClickListener(new View.OnLongClickListener() {
+        convertView.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onLongClick(View v) {
+            public void onClick(View v) {
                 int position = (Integer) v.getTag();
                 Route route = (Route) getItem(position);
-                Intent intent = new Intent(getContext(), MapsActivity.class);
+                Intent intent = new Intent(mContext, MapsActivity.class);
                 intent.putExtra("from", route.from);
                 intent.putExtra("fromName", route.fromName);
                 intent.putExtra("to", route.to);
                 intent.putExtra("toName", route.toName);
                 intent.putExtra("preview", true);
                 intent.putExtra("cycling", route.cycling);
-                return false;
+                Log.d("MYROUTES", route.routeJSON);
+                intent.putExtra("points", route.routeJSON);
+                mContext.startActivity(intent);
             }
         });
         return convertView;
